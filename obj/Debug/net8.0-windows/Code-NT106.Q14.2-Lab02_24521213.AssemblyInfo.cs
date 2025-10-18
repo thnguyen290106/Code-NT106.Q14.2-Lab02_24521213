af574553212d1714ae7ba56d50830ec0427cba3e
thnguyen290106 <thnguyen290106@gmail.com>
2025-10-18 17:19:55 +0700

    Added readme

--- obj/Debug/net8.0-windows/Code-NT106.Q14.2-Lab02_24521213.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Code-NT106.Q14.2-Lab02_24521213.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code-NT106.Q14.2-Lab02_24521213")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d53a84bf26586e36cd7870bd9a7a928ff5fd046b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b647d02c0981d3628d5d1aee7f0a55fda25793b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code-NT106.Q14.2-Lab02_24521213")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code-NT106.Q14.2-Lab02_24521213")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
